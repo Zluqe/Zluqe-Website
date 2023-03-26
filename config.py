b85c91646504d560a6167b3c7311f5cd3b4a0dcf
Person0z <80363995+Person0z@users.noreply.github.com>
2023-03-26 17:35:48 -0600

    update

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 GITHUB = "https://github.com/Zluqe/"
 DISCORD_INVITE = "https://discord.gg/D8rjRN3uJQ"
+PANEL_STORE_CREATE = "https://panel.zluqe.com/store/create"
+PANEL = "https://panel.zluqe.com/"
--- main.py
+++ main.py
@@ -5,17 +5,28 @@ import config
 
 app = Flask(__name__, static_folder='static', template_folder='views')
 
+# Render Templates / HTML's
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+# Redirects
+
 @app.route('/discord')
 def discord_invite():
     return redirect(config.DISCORD_INVITE, code=302)
-
 @app.route('/github')
 def github_link():
     return redirect(config.GITHUB, code=302)
+@app.route('/create')
+def zluqe_create():
+    return redirect(config.PANEL_STORE_CREATE, code=302)
+@app.route('/panel')
+def zluqe_panel():
+    return redirect(config.PANEL, code=302)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
